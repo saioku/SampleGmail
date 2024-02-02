ca8aa90f0d3799b86ba25fee763e8c794965335c
saioku <oku.saipranith@gmail.com>
2024-02-01 23:14:12 -0500

    some tweaks

--- app/src/main/java/com/example/samplegmail/MainActivity.kt
+++ app/src/main/java/com/example/samplegmail/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.example.samplegmail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.samplegmail.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var emails: List<Email>
+
+    private lateinit var emails: MutableList<Email>
+    private lateinit var emailAdapter: EmailAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val emailView= findViewById<RecyclerView>(R.id.recyclerView)
-        emails = EmailFetcher.getEmails()
-        val adapter = EmailAdapter(emails)
-        emailView.adapter = adapter
-        emailView.layoutManager = LinearLayoutManager(this)
-        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
-            val newEmails = EmailFetcher.getNext5Emails()
-            (emails as MutableList<Email>).addAll(newEmails)
-            adapter.notifyDataSetChanged()
-        }
 
+        setupRecyclerView()
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            loadMoreEmails()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val emailView = findViewById<RecyclerView>(R.id.recyclerView)
+        emails = EmailFetcher.getEmails().toMutableList()
+        emailAdapter = EmailAdapter(emails)
+        emailView.adapter = emailAdapter
+        emailView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun loadMoreEmails() {
+        val newEmails = EmailFetcher.getNext5Emails()
+        emails.addAll(newEmails)
+        emailAdapter.notifyDataSetChanged()
     }
 }
